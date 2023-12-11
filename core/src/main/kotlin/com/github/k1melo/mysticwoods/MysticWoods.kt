92036d46af5a4b6b99fe79b7489dde81b713fe05
K1Melo <kauafernandes.smelo@gmail.com>
2023-12-10 21:32:27 -0300

    feat: create project

---
+++ core/src/main/kotlin/com/github/k1melo/mysticwoods/MysticWoods.kt
@@ -0,0 +1,6 @@
+package com.github.k1melo.mysticwoods
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class MysticWoods : ApplicationAdapter()
---
+++ lwjgl3/src/main/kotlin/com/github/k1melo/mysticwoods/lwjgl3/Lwjgl3Launcher.kt
@@ -0,0 +1,19 @@
+@file:JvmName("Lwjgl3Launcher")
+
+package com.github.k1melo.mysticwoods.lwjgl3
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.github.k1melo.mysticwoods.MysticWoods
+
+/** Launches the desktop (LWJGL3) application. */
+fun main() {
+    // This handles macOS support and helps on Windows.
+    if (StartupHelper.startNewJvmIfRequired())
+      return
+    Lwjgl3Application(MysticWoods(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("MysticWoods")
+        setWindowedMode(640, 480)
+        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
+    })
+}
